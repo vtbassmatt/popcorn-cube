155c49c64fd6f4c5935cdbc042fcb5be78377cc4
Matt Cooper <vtbassmatt@gmail.com>
2026-05-27 08:51:13 -0400

    disable noisy mid-round emails

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -87,6 +87,8 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 EMAIL_FROM = config("EMAIL_FROM", default="no-one@example.org")
+# mid-round emails are noisy, so they're disabled by default
+EMAIL_SEND_MIDROUND = False
 
 
 AUTH_PASSWORD_VALIDATORS = [
